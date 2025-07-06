a5f4b2c568e88f64f04d8e49ba498abc83aad530
hhao99 <hhao99@gmail.con>
2025-07-05 19:17:19 -0500

    r is ready with nixos-25.05

--- flake.nix
+++ flake.nix
@@ -1,30 +1,27 @@
 {
     description = "A simple Nix package for a hypothetical application";
     inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
         flake-utils.url = "github:numtide/flake-utils";
     };
 
     outputs = { self, nixpkgs, flake-utils }:
-        flake-utils.lib.eachDefaultSystem (system:
-            let
+       flake-utils.lib.eachDefaultSystem( system:
+            let 
                 pkgs = import nixpkgs {
                     inherit system;
                 };
-            in
-            {
-                packages.default = pkgs.stdenv.mkDerivation {
-                    pname = "myapp";
-                    version = "1.0.0";
-                    src = ./.;
-                    buildInputs = with pkgs; [
-                        python3
-                        python3Packages.jupyterlab
-                      ];
-                    installPhase = ''
-                        mkdir -p $out/bin
-                        cp myapp $out/bin/
+                jupyterPackages = with pkgs; [
+                   R
+                    
+                ];
+            in {
+                devShells.default = pkgs.mkShell {
+                    packages = jupyterPackages;
+                    shellHook = ''
+                        echo "Welcome to the development shell for the hypothetical application!"
                     '';
                 };
-            });
+            }
+        );
 }
